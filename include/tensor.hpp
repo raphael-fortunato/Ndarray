e9190962fe9635da124dacb9d013dc37ddd61964
Raphael Furtunato <rfurtunato@lely.com>
2024-12-31 15:32:35 +0100

    Moved data to base class

---
+++ include/tensor.hpp
@@ -0,0 +1,76 @@
+/*
+ * This is the header file for the Ndarray class.
+ * This class is the base class of a multi-dimensional array.
+ * Every ndarray array needs a data_type T, shape, and strides.
+ */
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdio>
+#include <iterator>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+#include "tensor_base.hpp"
+#include "tensor_impl.hpp"
+
+namespace tensor {
+template <typename dtype, size_t N>
+class Tensor : public TensorBase<dtype, N> {
+   public:
+    using value_type = dtype;
+    explicit Tensor(tensor_impl::tensor_initializer<dtype, N> init) {
+        printf("Constructor\n");
+        this->m_shape = tensor_impl::derive_shape<N>(init);
+        m_compute_size();
+        m_allocate_data();
+        tensor_impl::insert_flat(init, this->m_data);
+        m_compute_strides();
+        assert(this->end() - this->begin() == this->size());
+    }
+    ~Tensor() override {
+        printf("Destructor\n");
+        delete[] this->m_data;
+    }
+    Tensor& operator=(const Tensor& other) {
+        printf("Copy assignment\n");
+        if (this != &other) {
+            delete[] this->m_data;
+            TensorBase<dtype, N>::operator=(std::move(other));
+        }
+        return *this;
+    }
+
+    Tensor(const Tensor& other) : TensorBase<dtype, N>(other) {
+        printf("Copy constructor\n");
+    }
+    Tensor(Tensor&& other) noexcept : TensorBase<dtype, N>(std::move(other)) {
+        printf("Move constructor\n");
+    }
+
+   private:
+    void m_allocate_data() {
+        this->m_data = new dtype[this->m_size];
+        this->m_end_itr = this->m_data + this->m_size;
+    }
+
+    void m_compute_size() {
+        this->m_size =
+            std::accumulate(this->m_shape.begin(), this->m_shape.end(), 1,
+                            std::multiplies<std::size_t>());
+    }
+    void m_compute_strides() {
+        this->m_strides.resize(this->m_shape.size());
+        if (this->m_strides.empty()) {
+            return;
+        }
+        this->m_strides.back() = 1;
+        for (int i = this->m_strides.size() - 2; i >= 0; --i) {
+            this->m_strides[i] = this->m_shape[i + 1] * this->m_strides[i + 1];
+        }
+    }
+};
+
+}  // namespace tensor
--- test/test_tensor_construction.cpp
+++ test/test_tensor_construction.cpp
@@ -3,35 +3,44 @@
 #include <vector>
 
 #include "gmock/gmock.h"
-#include "ndarray.hpp"
+#include "tensor.hpp"
 
-using namespace ndarray;
+using namespace tensor;
 
-TEST(NdarrayTest, TestShape) {
+TEST(TensorTest, TestEmptyArray) {
+    Tensor<double, 1> test_array{};
+    ASSERT_THAT(test_array.size(), ::testing::Eq(0));
+    ASSERT_EQ(test_array.end() - test_array.begin(), test_array.size());
+}
+TEST(TensorTest, TestShape) {
     // Test 1 dim array
-    Ndarray<double, 1> test_array = {12.5, 13.5};
+    Tensor<double, 1> test_array{12.5, 13.5};
     ASSERT_THAT(test_array, ::testing::ElementsAre(12.5, 13.5));
     ASSERT_THAT(test_array.shape(), ::testing::ElementsAre(2));
+    ASSERT_EQ(test_array.end() - test_array.begin(), test_array.size());
 
-    Ndarray<int, 2> test_array2 = {{12, 13}, {14, 15}};
+    Tensor<int, 2> test_array2{{12, 13}, {14, 15}};
     ASSERT_THAT(test_array2.shape(), ::testing::ElementsAre(2, 2));
+    ASSERT_EQ(test_array2.end() - test_array2.begin(), test_array2.size());
     ASSERT_THAT(test_array2, ::testing::ElementsAre(12, 13, 14, 15));
 
     // // Test 3 dim array
-    Ndarray<double, 3> test_array3 = {{{12.5, 13.5}, {14.5, 15.5}},
-                                      {{16.5, 17.5}, {18.5, 19.5}}};
+    Tensor<double, 3> test_array3{{{12.5, 13.5}, {14.5, 15.5}},
+                                  {{16.5, 17.5}, {18.5, 19.5}}};
     ASSERT_THAT(test_array3.shape(), ::testing::ElementsAre(2, 2, 2));
+    ASSERT_EQ(test_array3.end() - test_array3.begin(), test_array3.size());
     ASSERT_THAT(test_array3, ::testing::ElementsAre(12.5, 13.5, 14.5, 15.5,
                                                     16.5, 17.5, 18.5, 19.5));
 
     // Test array with shap 2x3x4
-    Ndarray<double, 3> test_array4 = {{{12.5, 13.5, 14.5, 15.5},
-                                       {16.5, 17.5, 18.5, 19.5},
-                                       {20.5, 21.5, 22.5, 23.5}},
-                                      {{24.5, 25.5, 26.5, 27.5},
-                                       {28.5, 29.5, 30.5, 31.5},
-                                       {32.5, 33.5, 34.5, 35.5}}};
+    Tensor<double, 3> test_array4{{{12.5, 13.5, 14.5, 15.5},
+                                   {16.5, 17.5, 18.5, 19.5},
+                                   {20.5, 21.5, 22.5, 23.5}},
+                                  {{24.5, 25.5, 26.5, 27.5},
+                                   {28.5, 29.5, 30.5, 31.5},
+                                   {32.5, 33.5, 34.5, 35.5}}};
     ASSERT_THAT(test_array4.shape(), ::testing::ElementsAre(2, 3, 4));
+    ASSERT_EQ(test_array4.end() - test_array4.begin(), test_array4.size());
     ASSERT_THAT(
         test_array4,
         ::testing::ElementsAre(12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5,
@@ -39,15 +48,16 @@ TEST(NdarrayTest, TestShape) {
                                28.5, 29.5, 30.5, 31.5, 32.5, 33.5, 34.5, 35.5));
 }
 
-TEST(NdarrayTest, TestSize) {
+TEST(TensorTest, TestSize) {
     // Test the size of an array with shape 2x3x4
-    Ndarray<double, 3> test_array = {{{12.5, 13.5, 14.5, 15.5},
-                                      {16.5, 17.5, 18.5, 19.5},
-                                      {20.5, 21.5, 22.5, 23.5}},
-                                     {{24.5, 25.5, 26.5, 27.5},
-                                      {28.5, 29.5, 30.5, 31.5},
-                                      {32.5, 33.5, 34.5, 35.5}}};
+    Tensor<double, 3> test_array{{{12.5, 13.5, 14.5, 15.5},
+                                  {16.5, 17.5, 18.5, 19.5},
+                                  {20.5, 21.5, 22.5, 23.5}},
+                                 {{24.5, 25.5, 26.5, 27.5},
+                                  {28.5, 29.5, 30.5, 31.5},
+                                  {32.5, 33.5, 34.5, 35.5}}};
     ASSERT_THAT(test_array.size(), ::testing::Eq(24));
+    ASSERT_EQ(test_array.end() - test_array.begin(), test_array.size());
     ASSERT_THAT(
         test_array,
         ::testing::ElementsAre(12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5,
@@ -55,39 +65,41 @@ TEST(NdarrayTest, TestSize) {
                                28.5, 29.5, 30.5, 31.5, 32.5, 33.5, 34.5, 35.5));
 
     // Test the size of an array with shape 2x2x2x2
-    Ndarray<double, 4> test_array2 = {
+    Tensor<double, 4> test_array2{
         {{{12.5, 13.5}, {14.5, 15.5}}, {{16.5, 17.5}, {18.5, 19.5}}},
         {{{20.5, 21.5}, {22.5, 23.5}}, {{24.5, 25.5}, {26.5, 27.5}}}};
     ASSERT_THAT(test_array2.size(), ::testing::Eq(16));
+    ASSERT_EQ(test_array2.end() - test_array2.begin(), test_array2.size());
     ASSERT_THAT(
         test_array2,
         ::testing::ElementsAre(12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5,
                                20.5, 21.5, 22.5, 23.5, 24.5, 25.5, 26.5, 27.5));
     // Test the size of an array with shape 1x1x3x3
-    Ndarray<double, 4> test_array3 = {
+    Tensor<double, 4> test_array3{
         {{{12.5, 13.5, 14.5}, {15.5, 16.5, 17.5}, {18.5, 19.5, 20.5}}}};
     ASSERT_THAT(test_array3.size(), ::testing::Eq(9));
+    ASSERT_EQ(test_array3.end() - test_array3.begin(), test_array3.size());
     ASSERT_THAT(test_array3,
                 ::testing::ElementsAre(12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5,
                                        19.5, 20.5));
 }
-TEST(NdarrayTest, TestCopyConstructor) {
-    Ndarray<double, 1> test_array1{2, 3, 4};
+TEST(TensorTest, TestCopyConstructor) {
+    Tensor<double, 1> test_array1{2, 3, 4};
     auto test_array2{test_array1};
     EXPECT_EQ(test_array1.size(), test_array2.size());
     EXPECT_EQ(test_array1.cend() - test_array1.cbegin(),
               test_array2.cend() - test_array2.cbegin());
 }
-TEST(NdarrayTest, TestCopyAssignment) {
-    Ndarray<double, 1> test_array1{2, 3, 4, 5};
-    Ndarray<double, 1> test_array2 = {5, 6, 7};
+TEST(TensorTest, TestCopyAssignment) {
+    Tensor<double, 1> test_array1{2, 3, 4, 5};
+    Tensor<double, 1> test_array2{5, 6, 7};
     test_array2 = test_array1;
     EXPECT_EQ(test_array1.size(), test_array2.size());
     EXPECT_EQ(test_array1.cend() - test_array1.cbegin(),
               test_array2.cend() - test_array2.cbegin());
 }
-TEST(NdarrayTest, TestMoveConstructor) {
-    Ndarray<double, 1> test_array1{2, 3, 4};
+TEST(TensorTest, TestMoveConstructor) {
+    Tensor<double, 1> test_array1{2, 3, 4};
     auto test_array2 = std::move(test_array1);
     EXPECT_EQ(test_array1.begin(), nullptr);
     EXPECT_EQ(test_array1.end(), nullptr);
@@ -96,22 +108,22 @@ TEST(NdarrayTest, TestMoveConstructor) {
     EXPECT_EQ(test_array2.cend() - test_array2.cbegin(), test_array2.size());
 }
 
-TEST(NdarrayTest, TestStrides) {
-    Ndarray<double, 3> test_array = {{{12.5, 13.5, 14.5, 15.5},
-                                      {16.5, 17.5, 18.5, 19.5},
-                                      {20.5, 21.5, 22.5, 23.5}},
-                                     {{24.5, 25.5, 26.5, 27.5},
-                                      {28.5, 29.5, 30.5, 31.5},
-                                      {32.5, 33.5, 34.5, 35.5}}};
+TEST(TensorTest, TestStrides) {
+    Tensor<double, 3> test_array{{{12.5, 13.5, 14.5, 15.5},
+                                  {16.5, 17.5, 18.5, 19.5},
+                                  {20.5, 21.5, 22.5, 23.5}},
+                                 {{24.5, 25.5, 26.5, 27.5},
+                                  {28.5, 29.5, 30.5, 31.5},
+                                  {32.5, 33.5, 34.5, 35.5}}};
     ASSERT_THAT(test_array.strides(), ::testing::ElementsAre(12, 4, 1));
 
-    Ndarray<int, 1> test_array2{2, 3, 4, 5};
+    Tensor<int, 1> test_array2{2, 3, 4, 5};
     ASSERT_THAT(test_array2.strides(), ::testing::ElementsAre(1));
 
-    Ndarray<char, 5> test_array3{{{{{{1, 2, 3, 4, 5}}}}}};
+    Tensor<char, 5> test_array3{{{{{{1, 2, 3, 4, 5}}}}}};
     ASSERT_THAT(test_array3.strides(), ::testing::ElementsAre(5, 5, 5, 5, 1));
 
-    Ndarray<int, 4> test_array4{{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}},
-                                {{{9, 10}, {11, 12}}, {{13, 14}, {15, 16}}}};
+    Tensor<int, 4> test_array4{{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}},
+                               {{{9, 10}, {11, 12}}, {{13, 14}, {15, 16}}}};
     ASSERT_THAT(test_array4.strides(), ::testing::ElementsAre(8, 4, 2, 1));
 }
