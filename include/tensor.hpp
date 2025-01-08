63c9cb9ab68b30bb3ac176b23b95acebd97742ed
Raphael Furtunato <rfurtunato@lely.com>
2025-01-08 11:27:57 +0100

    Moved functions out of Tensor class

--- include/tensor.hpp
+++ include/tensor.hpp
@@ -24,10 +24,10 @@ class Tensor : public TensorBase<dtype, N> {
     explicit Tensor(tensor_impl::tensor_initializer<dtype, N> init) {
         printf("Constructor\n");
         this->m_shape = tensor_impl::derive_shape<N>(init);
-        this->m_size = tensor_impl::m_compute_size(this->m_shape);
+        this->m_size = m_compute_size(this->m_shape);
         m_allocate_data();
         tensor_impl::insert_flat(init, this->m_data);
-        this->m_strides = tensor_impl::m_compute_strides(this->m_shape);
+        this->m_strides = m_compute_strides(this->m_shape);
         assert(this->end() - this->begin() == this->size());
     }
 
@@ -36,9 +36,9 @@ class Tensor : public TensorBase<dtype, N> {
     explicit Tensor(Args... args) {
         printf("Constructor\n");
         this->m_shape = {static_cast<std::size_t>(args)...};
-        this->m_size = tensor_impl::m_compute_size(this->m_shape);
+        this->m_size = m_compute_size(this->m_shape);
         m_allocate_data();
-        this->m_strides = tensor_impl::m_compute_strides(this->m_shape);
+        this->m_strides = m_compute_strides(this->m_shape);
         assert(this->end() - this->begin() == this->size());
     }
 
@@ -58,62 +58,28 @@ class Tensor : public TensorBase<dtype, N> {
     Tensor(const Tensor& other) : TensorBase<dtype, N>(other) {
         printf("Copy constructor\n");
     }
-    Tensor(Tensor&& other) noexcept : TensorBase<dtype, N>(std::move(other)) {
+    Tensor(Tensor&& other) noexcept
+        : TensorBase<dtype, N>(std::forward<Tensor>(other)) {
         printf("Move constructor\n");
     }
+
     Tensor& operator=(Tensor&& other) noexcept {
         printf("Move assignment\n");
         if (this != &other) {
             delete[] this->m_data;
-            TensorBase<dtype, N>::operator=(std::move(other));
+            TensorBase<dtype, N>::operator=(std::forward<Tensor>(other));
         }
         return *this;
     }
-    template <typename... Args>
-        requires tensor_impl::ValidateTensorRefReturn<N, Args...>
-    TensorRef<dtype, N - sizeof...(Args)> operator()(Args&&... args) {
-        static_assert(sizeof...(args) < N, "Invalid number of arguments");
-        std::array<std::size_t, sizeof...(args)> indices{
-            static_cast<std::size_t>(args)...};
-
-        std::vector<std::size_t> new_shape(
-            this->m_shape.begin() + indices.size(), this->m_shape.end());
-        std::vector<std::size_t> new_strides(
-            this->m_strides.begin() + indices.size(), this->m_strides.end());
-
-        std::size_t offset = 0;
-        for (std::size_t i = 0; i < indices.size(); ++i) {
-            assert(indices[i] < this->m_shape[i] &&
-                   "Index out of bounds: index exceeds shape dimension");
-            offset += indices[i] * this->m_strides[i];
-        }
-        return TensorRef<dtype, N - sizeof...(Args)>(this->m_data + offset,
-                                                     std::move(new_shape),
-                                                     std::move(new_strides));
-    }
-
-    template <typename... Args>
-        requires tensor_impl::ValidateElementReturn<N, Args...>
-    dtype operator()(Args&&... args) {
-        static_assert(sizeof...(args) == N, "Invalid number of arguments");
-        const std::array<std::size_t, N> indices{
-            static_cast<std::size_t>(args)...};
-        std::size_t linear_index = 0;
-        for (std::size_t i = 0; i < N; ++i) {
-            if (indices[i] >= this->m_shape[i]) {
-                throw std::out_of_range("Index out of range");
-            }
-            linear_index += indices[i] * this->m_strides[i];
-        }
-
-        return this->m_data[linear_index];
-    }
 
    private:
-    void m_allocate_data() {
-        this->m_data = new dtype[this->m_size];
-        this->m_end_itr = this->m_data + this->m_size;
-    }
+    void m_allocate_data();
 };
+
+template <typename dtype, std::size_t N>
+void Tensor<dtype, N>::m_allocate_data() {
+    this->m_data = new dtype[this->m_size];
+    this->m_end_itr = this->m_data + this->m_size;
+}
 
 }  // namespace tensor
--- include/tensor_ref.hpp
+++ include/tensor_ref.hpp
@@ -5,13 +5,14 @@ namespace tensor {
 template <typename dtype, std::size_t N>
 class TensorRef : public TensorBase<dtype, N> {
    public:
-    TensorRef() {};
+    TensorRef() = delete;
+
     TensorRef(dtype* data, std::vector<std::size_t> shape,
               std::vector<std::size_t> strides) {
         this->m_data = data;
         this->m_shape = std::forward<std::vector<std::size_t>>(shape);
         this->m_strides = std::forward<std::vector<std::size_t>>(strides);
-        this->m_size = tensor_impl::m_compute_size(this->m_shape);
+        this->m_size = m_compute_size(this->m_shape);
         this->m_end_itr = data + this->m_size;
     }
 };
